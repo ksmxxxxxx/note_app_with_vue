0b035946446d7c48db01ac65ade8eaf5ef3dae82
ksmxxxxxx <kankichi69@gmail.com>
2021-04-04 15:07:27 +0900

    wip

---
+++ src/components/NoteDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+    <div>{{ body }}</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NoteDetail',
+  inject: ['items'],
+  props: ['title', 'body'],
+  created () {
+    console.log(this)
+  }
+}
+</script>

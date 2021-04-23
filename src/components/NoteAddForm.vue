363486050ba7d74c2e0606514d7188cf36237bb4
ksmxxxxxx <kankichi69@gmail.com>
2021-04-23 12:37:49 +0900

    :wrench: Fix based on comments

--- src/components/NoteAddForm.vue
+++ src/components/NoteAddForm.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="flex">
       <label for="title" class="flex-none w-14">Title</label>
-      <input id="title" type="text" v-model="newNote.title" class="flex-grow">
+      <input id="title" type="text" v-model="title" class="flex-grow">
     </div>
     <div class="flex mt-4">
       <label for="body" class="flex-none w-14">Body</label>
-      <textarea id="body" v-model="newNote.body" class="flex-grow"></textarea>
+      <textarea id="body" v-model="body" class="flex-grow"></textarea>
     </div>
     <div class="flex justify-center mt-4">
       <button @click="cancelAdd" class="inline-flex px-6 py-3 rounded-full bg-gray-100">Cancel</button>
-      <button @click="addNote" :disabled="disabled" class="disabled:opacity-50 inline-flex ml-4 px-6 py-3 rounded-full ring-4 ring-indigo-300 bg-indigo-700 text-white">Save</button>
+      <button @click="addNote" :disabled="disabled" class="disabled:opacity-80 inline-flex ml-4 px-6 py-3 rounded-full ring-4 ring-indigo-300 bg-indigo-700 text-white">Save</button>
     </div>
   </div>
 </template>
@@ -20,16 +20,15 @@ export default {
   data () {
     return {
       notes: [],
-      newNote: {
-        title: null,
-        body: null,
-        edit: false
-      },
+      newNote: {},
+      title: null,
+      body: null,
+      edit: false,
       disabled: true
     }
   },
   watch: {
-    'newNote.title': function () {
+    title () {
       this.activateSave()
     }
   },
@@ -44,7 +43,7 @@ export default {
   },
   methods: {
     activateSave () {
-      if (this.newNote.title) {
+      if (this.title) {
         this.disabled = false
       } else {
         this.disabled = true
@@ -54,12 +53,15 @@ export default {
       this.$router.push({ path: '/' })
     },
     addNote () {
-      if (!this.newNote) {
+      if (!this.title) {
         return
       }
+      this.newNote.title = this.title
+      this.newNote.body = this.body
+      this.newNote.edit = this.edit
       this.notes.push(this.newNote)
-      this.newNote = ''
       this.saveNote()
+      this.newNote = ''
       this.$router.push({ path: '/' })
     },
     saveNote () {
